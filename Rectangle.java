1f0710a5a38e23d6fb5cb7dfee1a7405546ae11e
sandeepbysani <bysani.sandeep@gmail.com>
2015-05-26 08:25:20 +0530

    Added Rect & Tri class files

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle extends Figure{
+	Rectangle(double a, double b){
+		super(a,b);
+	}
+
+	double area(){
+		System.out.println("Inside Area for Rectangle");
+		return dim1 * dim2;
+	}
+}
